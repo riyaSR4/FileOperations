277fcb0810ec2fcc0b045b42e2b0d94b3f49d2b7
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-11 11:02:28 +0530

    [Riya] Add : FileOperations

---
+++ FileOperations/Operations.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperations
+{
+    public class Operations
+    {
+        public static void FileExists(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File Exists");
+            }
+        }
+        public static void ReadAllLines(string filePath) 
+        {
+            if(File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (var data in lines)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File doesnt exists");
+            }
+        }
+        public static void ReadAllText(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string lines = File.ReadAllText(filePath);
+                Console.WriteLine(lines);
+            }
+            else
+            {
+                Console.WriteLine("File doesnt exists");
+            }
+        }
+        public static void CreateFile(string filePath)
+        {
+            File.Create(filePath);
+        }
+        public static void CopyFilePath(string existing, string copyFilePath)
+        {
+            File.Copy(existing, copyFilePath);
+        }
+        public static void DeleteFile(string filePath)
+        {
+            File.Delete(filePath);
+        }
+        public static void ReadFromStreamReader(string filePath)
+        {
+            using(StreamReader reader = File.OpenText(filePath)) 
+            {
+                string s = "";
+                while((s = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        public static void WriteFromStreamWriter(string filePath)
+        {
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine("Hello World! .NET is Awesome");
+                writer.Close();
+                ReadAllText(filePath);
+            }
+        }
+    }
+}
---
+++ FileOperations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FileOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\BridgeLabs Training\FileOperations\FileOperations\Sample.txt";
+            string createFilePath = @"D:\BridgeLabs Training\FileOperations\FileOperations\Create.txt";
+            string copyFilePath = @"D:\BridgeLabs Training\FileOperations\FileOperations\Copy.txt";
+            Operations.FileExists(filePath);
+            Operations.ReadAllLines(filePath);
+            Operations.ReadAllText(filePath);
+            Operations.CreateFile(createFilePath);
+            Operations.CopyFilePath(filePath, copyFilePath);
+            Operations.DeleteFile(copyFilePath);
+            Operations.ReadFromStreamReader(filePath);
+            Operations.WriteFromStreamWriter(filePath);
+        }
+    }
+}
